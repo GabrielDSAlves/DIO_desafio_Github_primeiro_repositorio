156b3f51005527fd249d44fd9e76825bd9b29387
GabrielDSAlves <gabrieldsa1609@gmail.com>
2022-01-06 00:42:53 -0300

    Desafio - Meu primeiro repositório

---
+++ Primeiro_repositorio.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(void)
+{
+
+    cout << "--------------------------" << endl;
+    cout << "   Desafio meu primeiro   " << endl;
+    cout << "      repositório         " << endl;
+    cout << "--------------------------" << endl;
+
+    cin.get();
+    return 0;
+}
